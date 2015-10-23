a2133f411df21cbd94f7f5ce4fa03e35f9d47036
thaiduchyk <thaiduchyk@gmail.com>
2015-10-23 21:19:27 +0300

    Created Player

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Player {
+
+	String name;
+
+	public Player(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+}
